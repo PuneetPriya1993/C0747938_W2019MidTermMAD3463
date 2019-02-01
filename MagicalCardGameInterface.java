161e1d74a62924f1ee5a8c2971832b67a33288f3
Priya Aggarwal <aggarwalpriya.9877@gmail.com>
2019-01-31 20:23:17 -0500

    UpdatedMidTermW2019

--- MagicalCardGameInterface.java
+++ MagicalCardGameInterface.java
@@ -3,6 +3,8 @@ package com.lambton.magicalcardgame;
 interface MagicalCardGameModelInterface
 {
     public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel);
+
     public String[][] getSecShuffleResult(MagicalCardGameModel magicalCardGameModel);
+
     public String getFinalResult(MagicalCardGameModel magicalCardGameModel);
 }
