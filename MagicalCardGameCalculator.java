f77833b6fda933768cddab1b9c8a2b343f8fa58e
Priya Aggarwal <aggarwalpriya.9877@gmail.com>
2019-01-31 19:32:33 -0500

    MidTerm

---
+++ MagicalCardGameCalculator.java
@@ -0,0 +1,14 @@
+package com.lambton.magicalcardgame;
+
+
+public class MagicalCardGameCalculator
+{
+    public static void main(String[] args)
+    {
+        MagicalCardGameModel magicalCardGameModel=new MagicalCardGameModel();
+        
+        public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel),
+        public String[][] getSecShuffleResult (MagicalCardGameModel magicalCardGameModel),
+        public String getFinalResult (MagicalCardGameModel magicalCardGameModel)
+    }
+}
---
+++ MagicalCardGameInterface.java
@@ -0,0 +1,12 @@
+package com.lambton.magicalcardgame;
+
+interface MagicalCardGameInterface
+{
+    public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel);
+
+    public String[][] getSecShuffleResult(MagicalCardGameModel magicalCardGameModel);
+
+    public String getFinalResult(MagicalCardGameModel magicalCardGameModel);
+
+
+}
