7e72fb7a31ba958e803b9646b290c7a4dbf2a777
Priya Aggarwal <aggarwalpriya.9877@gmail.com>
2019-01-31 21:14:52 -0500

    UpdatedMidTermW2019

--- MagicalCardGameCalculator.java
+++ MagicalCardGameCalculator.java
@@ -16,9 +16,17 @@ public class MagicalCardGameCalculator
         if(firstShuffleColPos.contains("A"))
         {
                 rowNumber =0;
-                tempArray[0][0]=cards1[0][1];
-                tempArray[0][1]=cards1[1][1];
-                tempArray[0][2]=cards1[2][1];
+            tempArray[0][0]=cards1[0][1];
+            tempArray[0][1]=cards1[1][1];
+            tempArray[0][2]=cards1[2][1];
+
+            tempArray[1][0]=cards1[0][0];
+            tempArray[1][1]=cards1[1][0];
+            tempArray[1][2]=cards1[2][0];
+
+            tempArray[2][0]=cards1[0][2];
+            tempArray[2][1]=cards1[1][2];
+            tempArray[2][2]=cards1[2][2];
 
                 for (int w=0; w<cards1.length; w++)
                 {
